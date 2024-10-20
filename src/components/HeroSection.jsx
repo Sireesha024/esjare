f95bce625067da095d9160ecf8d2c200aa130d6f
divya <divyanavale1999@gmail.com>
2024-10-19 23:59:04 -0400

    image and home page styling

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import './../css/heroSection.css'
 
 const HeroSection = () => {
   return (
-    <section id="home">
-      <h1>Welcome to Emerovate</h1>
-      <p>Your Trusted IT Managed Services Provider</p>
-      <button>Get Started</button>
+    <section className="home">
+      <div className="description">
+        <p>
+          Welcome to Emerovate- Trusted IT Managed Services Provider, we are based in Illinois, USA. Our staff work both onsite and remote. The Companies that we support are: Health Care, Legal, Manufacturing, Education, Non-Profit Organizations, Construction, Financial Sector, Retails, Government Agencies, Real Estate, Restaurants, and more.
+        </p>
+      </div>
     </section>
   );
 }
